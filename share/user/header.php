7e6fb1c32ebe3daef4efe291c63f6ab38547676c
chamberezigbo <chamberezigbo@gmail.com>
2024-08-26 18:54:38 +0100

    first commit

---
+++ share/user/header.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="en" itemscope itemtype="http://schema.org/WebPage">
+
+<head>
+     <!-- Required meta tags -->
+     <meta charset="utf-8">
+          <link rel="profile" href="https://gmpg.org/xfn/11">
+          <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+    />
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     <title> Forfits - Register|Login</title>
+     <meta name="title" Content="Creativewealth - Register">
+     <meta property="og:title" content="Defi &#8211; Pros" />
+     <meta property="og:description" content=" Forfits Investment has some flexible packages to accommodate all investors,no matter their financial status." />
+     <meta property="og:image" content="https://defiprosolutions.com/wp-content/uploads/2023/01/creatlog.png" />
+
+     <!-- my -->
+     <!-- toaster -->
+     <script src="../../wp-content/process/jquery.min.js"></script>
+     <link rel="stylesheet" href="../../wp-content/process/toastr-master/build/toastr.min.css">
+     <!--  -->
+     <link rel="apple-touch-icon" href="../assets/images/logoIcon/logo.png">
+     <meta name="apple-mobile-web-app-capable" content="yes">
+     <meta name="apple-mobile-web-app-status-bar-style" content="black">
+     <meta name="apple-mobile-web-app-title" content="Creativewealth - Register">
+
+     <meta itemprop="name" content="Creativewealth - Register">
+     <meta itemprop="description" content="Forfits Investment has some flexible packages to accommodate all investors,no matter their financial status.">
+     <meta itemprop="image" content="https://defiprosolutions.com/wp-content/uploads/2023/01/creatlog.png">
+
+     <!-- Bootstrap CSS -->
+
+     <link href="../assets/global/css/all.min.css" rel="stylesheet">
+
+     <link rel="stylesheet" href="../assets/global/css/line-awesome.min.css" />
+
+     <link rel="stylesheet" href="../assets/templates/bit_gold/css/vendor/animate.min.css">
+
+     <!-- slick slider css -->
+     <link rel="stylesheet" href="../assets/templates/bit_gold/css/vendor/slick.css">
+     <link rel="stylesheet" href="../assets/templates/bit_gold/css/vendor/dots.css">
+
+     <link rel="stylesheet" href="../assets/templates/bit_gold/css/main.css">
+
+     <link rel="stylesheet" href="../assets/templates/bit_gold/css/custom.css">
+     <style>
+          .country-code .input-group-text {
+               background: #fff !important;
+          }
+
+          .country-code select {
+               border: none;
+          }
+
+          .country-code select:focus {
+               border: none;
+               outline: none;
+          }
+
+
+          .base--color {
+               color: white;
+          }
+     </style>
+
+     <link rel="stylesheet" href="../assets/templates/bit_gold/css/colora100.css?color=CCA354">
+</head>
+
+<body>
+     <div class="preloader">
+          <div class="preloader__imges">
+               <img src="../assets/images/frontend/preloader/63c91bd834d561674124248.png" class="preloader__img one" alt="No Image">
+               <img src="../assets/images/frontend/preloader/63c91bd838a841674124248.png" class="preloader__img two" alt="No Image">
+          </div>
+     </div>
+
+     <!-- scroll-to-top start -->
+     <div class="scroll-to-top">
+          <span class="scroll-icon">
+               <i class="fa fa-rocket" aria-hidden="true"></i>
+          </span>
+     </div>
+     <!-- scroll-to-top end -->
+
+     <div class="full-wh">
+          <!-- STAR ANIMATION -->
+          <div class="bg-animation">
+               <div id='stars'></div>
+               <div id='stars2'></div>
+               <div id='stars3'></div>
+               <div id='stars4'></div>
+          </div><!-- / STAR ANIMATION -->
+     </div>
+
+
+
+
+     <div class="page-wrapper">
+    <!-- header-section start  -->
+<header class="header">
+    <div class="header__bottom">
+        <div class="container">
+            <nav class="navbar navbar-expand-xl p-0 align-items-center">
+                <a href="../index.php" class="navbar-brand"><img src="../assets/images/logoIcon/logo.png" alt="Logo" width="80" height="80" class="logo"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav main-menu ms-auto">
+                        <li class="nav-item"> <a class="nav-link" href="../../index.php">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="../../about/index.php">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="../plan.php">Plan</a></li>
+                        <li class="nav-item"><a class="nav-link" href="../../contacts/index.php">Contact</a></li>
+                    </ul>
+                    <div class="nav-right">
+                        <ul class="navbar-nav account-menu ms-3">
+                            <li class="nav-item icon"><a class="nav-link" href="login.php"><i class="las la-user"></i></a></li>
+                        </ul>
+                        <select class="select d-inline-block w-auto ms-xl-3 langSel">
+                            <option value="en" selected>English</option>
+                        </select>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div><!-- header__bottom end -->
+</header>
